a60dc914c859b3b7359d2fff36b68105ed7eb951
Jeremy Warner <jeremywrnr@gmail.com>
2015-09-24 04:31:32 -0400

    rename folder

--- cmplt/completion.bash
+++
@@ -1,9 +0,0 @@
-_web_complete()
-{
-  COMPREPLY=()
-  local word="${COMP_WORDS[COMP_CWORD]}"
-  local completions="$(web --cmplt "$word")"
-  COMPREPLY=( $(compgen -W "$completions" -- "$word") )
-}
-
-complete -f -F _web_complete web
--- cmplt/completion.zsh
+++
@@ -1,9 +0,0 @@
-_web_complete()
-{
-  local word completions
-  word="$1"
-  completions="$(web --complete "${word}")"
-  reply=( "${(ps:\n:)completions}" )
-}
-
-compctl -K _web_complete web
