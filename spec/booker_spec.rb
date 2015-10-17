cd46b64a5199751c1620110f662bdeecd847548a
Jeremy Warner <jeremywrnr@gmail.com>
2015-10-17 15:06:41 -0400

    adding better spec

--- spec/booker_spec.rb
+++ spec/booker_spec.rb
@@ -5,19 +5,39 @@ require "spec_helper"
 
 
 describe Booker do
-  def run_booker()
+  def run(str)
+    @booker = Booker.new(str.split)
   end
 
-  it "can read json file of bookmarks" do
+  it "should exit cleanly when no arguments are given" do
+    expect { run("") }.to raise_error SystemExit
+  end
+
+  it "should refute unrecognized flags" do
+    expect { run("--hello") }.to raise_error SystemExit
+  end
+
+  it "should accept valid option flags" do
+    valid_opts = %w{--version -v --install -i
+    --complete -c --bookmark -b --search -s}
+    valid_opts.each do |opt|
+      expect { run(opt) }.to raise_error SystemExit
+    end
   end
 end
 
 
 describe Bookmarks do
   before do
-    @bookmarks = JSON.parse(open('./spec/bookmarks.json').read)['roots']['bookmark_bar']['children']
+    @rawjson = JSON.parse(open('./spec/bookmarks.json').read)
+    @bookmarks = @rawjson['roots']['bookmark_bar']['children']
   end
 
-  it "parses json file correctly" do
+  it "parses json files correctly" do
+    expect(@rawjson).to_not be_nil
+  end
+
+  it "parses bookmarks correctly" do
+    expect(@bookmarks).to_not be_nil
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,18 @@ require 'rubygems'
 require 'booker'
 require 'rspec'
 require 'json'
+
+
+# Redirect stderr and stdout while testing
+RSpec.configure do |config|
+  original_stderr = $stderr
+  original_stdout = $stdout
+  config.before(:all) do
+    $stderr = File.open(File::NULL, "w")
+    $stdout = File.open(File::NULL, "w")
+  end
+  config.after(:all) do
+    $stderr = original_stderr
+    $stdout = original_stdout
+  end
+end
